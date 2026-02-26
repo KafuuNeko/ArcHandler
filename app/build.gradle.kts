e6fb5ee83d96f3e683e12ca9d107cf9bdf3f5d3f
Kafuu Neko <kafuuneko@gmail.com>
2026-02-26 14:16:08 +0800

    chore: splits abi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,19 @@ android {
                 cppFlags += ""
             }
         }
+
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+        }
+    }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
     }
 
     buildTypes {
